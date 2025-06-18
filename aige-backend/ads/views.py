6d6783c8d60caf5779492ec78fe17072cf2dd406
Vihaan Gautam <vihaangautam04@gmail.com>
2025-06-18 17:30:13 +0530

    edit StoryAdConfigForm backend

--- aige-backend/ads/views.py
+++ aige-backend/ads/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .models import Scene
 from .serializers import SceneSerializer
+from .models import AdConfiguration
+from .serializers import AdConfigurationSerializer
 
 class SceneViewSet(viewsets.ModelViewSet):
     queryset = Scene.objects.all()
     serializer_class = SceneSerializer
+
+class AdConfigurationViewSet(viewsets.ModelViewSet):
+    queryset = AdConfiguration.objects.all()
+    serializer_class = AdConfigurationSerializer
--- aige-backend/aige/urls.py
+++ aige-backend/aige/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ads.views import SceneViewSet
+from ads.views import AdConfigurationViewSet
+
 
 router = DefaultRouter()
 router.register(r'scenes', SceneViewSet)
+router.register(r'configs', AdConfigurationViewSet)  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
