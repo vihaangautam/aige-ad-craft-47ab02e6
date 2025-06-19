77bd47851362efc3bbfc572e531023bdc1f217e3
Vihaan Gautam <vihaangautam04@gmail.com>
2025-06-19 14:27:09 +0530

    added urls

--- aige-backend/aige/urls.py
+++ aige-backend/aige/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ads.views import SceneViewSet
-from ads.views import AdConfigurationViewSet
-
+from ads.views import SceneViewSet, AdConfigurationViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'scenes', SceneViewSet)
-router.register(r'configs', AdConfigurationViewSet)  
+router.register(r'configs', AdConfigurationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
