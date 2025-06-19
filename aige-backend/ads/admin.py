e854cb2bd27eab22f3bcccfa91b2fa7333e96169
Vihaan Gautam <vihaangautam04@gmail.com>
2025-06-19 14:25:30 +0530

    edits made

--- aige-backend/ads/admin.py
+++ aige-backend/ads/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Scene, AdConfiguration, UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+# Unregister original and re-register with profile inline
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+@admin.register(Scene)
+class SceneAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'label_a', 'label_b', 'created_at')
+
+@admin.register(AdConfiguration)
+class AdConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'theme_prompt', 'tone', 'enable_ar_filters', 'include_mini_game', 'created_at')
+
+admin.site.register(UserProfile)
--- aige-backend/ads/apps.py
+++ aige-backend/ads/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AdsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ads'
+
+    def ready(self):
+        import ads.signals
