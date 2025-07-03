c2fe3fb3887039c1afbd30ee033e44cc4fdec054
Vihaan Gautam <vihaangautam04@gmail.com>
2025-07-03 11:31:08 +0530

    video handling in backend

--- aige-backend/aige/urls.py
+++ aige-backend/aige/urls.py
@@ -4,6 +4,8 @@ from ads.views import ScriptGenerationView
 from rest_framework.routers import DefaultRouter
 from ads.views import SceneViewSet, AdConfigurationViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'scenes', SceneViewSet, basename='scene')
@@ -15,5 +17,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/generate-script/", ScriptGenerationView.as_view(), name="generate-script"),
+    path('ads/', include('ads.urls')),
 ]
+
+if settings.DEBUG:
+       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
